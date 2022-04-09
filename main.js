f596ddc4196cfc6498edeba887e59b7b9ccb1ade
Owais <87132814+captain0owais@users.noreply.github.com>
2022-04-09 16:34:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+song1 = "";
+song2 = "";
+
+function preload(){
+    song1 = loadSound("song1.mp3");
+    song2 = loadSound("song2.mp3");
+}
+
 function setup(){
     canvas = createCanvas(400 , 400);
     canvas.center();
