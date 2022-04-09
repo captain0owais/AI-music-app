dbc754f1e2b0665413798e5b299f907df81b8582
Owais <87132814+captain0owais@users.noreply.github.com>
2022-04-09 13:51:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup(){
+    canvas = createCanvas(400 , 400);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(400 , 400);
+    video.hide();
+}
+
+function draw(){
+    image(video , 0 , 0 , 400 , 400);
+}
