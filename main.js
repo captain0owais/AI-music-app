a632ab6a1aa8141b35548ce8a3e799c46444f386
Owais <87132814+captain0owais@users.noreply.github.com>
2022-04-22 14:09:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 song1 = "";
 song2 = "";
 
-leftwristX = 0;
-leftwristY = 0;
-rightwristX = 0;
-rightwristY = 0;
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristScore = 0;
+rightWristScore = 0;
+
+song1_status = "";
+song2_status = "";
 
 function preload(){
     song1 = loadSound("song1.mp3");
@@ -12,7 +18,7 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(400 , 400);
+    canvas = createCanvas(600 , 500);
     canvas.center();
     video = createCapture(VIDEO);
     video.size(400 , 400);
@@ -34,16 +40,43 @@ function gotResult(results){
         rightWristY = results[0].pose.rightWrist.y;
         console.log("LEFT WRIST X = " + leftWristX + "LEFT WRIST Y = " + leftWristY);
         console.log("RIGHT WRIST X = " + rightWristX + "RIGHT WRIST Y = " + rightWristY);
+        leftWristScore = results[0].pose.keypoints[9].score;
+        rightWristScore = results[0].pose.keypoints[10].score;
     }
 }
 
 function draw(){
-    image(video , 0 , 0 , 400 , 400);
+    image(video , 0 , 0 , 600 , 500);
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+
+    fill("#FF0000");
+    stroke("#FF0000");
+    circle(leftWristX , leftWristY , 20);
+    fill("#FF0000");
+    stroke("#FF0000");
+    circle(rightWristX , rightWristY , 20);
+
+    if(rightWristScore > 0.2){
+        song2.stop();
+        if(song2_status == false){
+            song1.play();
+            document.getElementById("song").innerHTML="2st song is playing";
+        }
+        console.log("playing song ---2️⃣---");
+    }
+
+    if(leftWristScore > 0.2){
+        song1.stop();
+    if(song1_status == false){
+        song2.play();
+        document.getElementById("song").innerHTML="1st song is playing";
+    }
+    console.log("playing song ---1️⃣---");
+    }
 }
 
 function start(){
-    console.log("playing song 2");
     song2.play();
-    song.rate(1);
-    song.setVolume(1);
 }
